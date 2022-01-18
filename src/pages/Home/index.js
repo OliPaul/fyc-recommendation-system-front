599bd075f15cfaa2eebd299e8077f1ddc6f4c796
Paul Olivier AHOUA <ahouapaulolivier@gmail.com>
2022-01-18 09:55:03 +0100

    TP solution

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -5,6 +5,7 @@ import Card from "../../components/Card";
 import animesClustered from "../../models/anime_clustered.json";
 import animeLinked from "../../models/anime_linked.csv";
 import animesDataset from "../../models/anime_dataset.json";
+import animeEpisode from "../../models/anime_episode.json";
 
 const Home = () => {
     const [isDetails, setIsDetails] = useState();
@@ -12,6 +13,8 @@ const Home = () => {
     const [neighbors, setNeighbors] = useState([]);
     const [clusters, setClusters] = useState([]);
     const [mostViewed, setMostViewed] = useState([]);
+    //const [allMostViewed, setAllMostViewed] = useState([]);
+    const [inSameEpisodeCluster, setInSameEpisodeCluster] = useState([]);
 
     const contentBasedRecommendation = (animeId) => {
         const animeInNeighbors = animesClustered.find(anime => anime.anime_id === animeId)
@@ -49,7 +52,7 @@ const Home = () => {
 
     }
 
-    const userBasedRecommendation = async (animeId) => {
+    const userBasedRecommendation = async (animeId, minViewers = 10000, callback = null) => {
         const file = animeLinked;
 
         const papaConfig = {
@@ -62,12 +65,16 @@ const Home = () => {
 
                 data.slice(1).forEach(lines => {
                     lines.map(((line, index) => {
-                        if (lines[0] !== animeId && index === selectedAnimeIndex && lines[index] > 10000) {
+                        if (lines[0] !== animeId && index === selectedAnimeIndex && lines[index] > minViewers) {
                             mostViewedByOthers.push(lines[0]);
                         }
                     }));
                 });
                 const animeMostViewedByOthers = animesDataset.filter(anime => mostViewedByOthers.includes(anime.anime_id.toString()));
+                if(callback) {
+                    callback(animeMostViewedByOthers);
+                    return;
+                }
                 setMostViewed(randomAndLimitArray(animeMostViewedByOthers, 6));
             },
             download: true,
@@ -77,6 +84,24 @@ const Home = () => {
         };
 
         await readString(file, papaConfig);
+    }
+    
+    const episodeAgeAndUsersBasedRecommendation = async (animeId) => {
+        const selectedAnime = animeEpisode.find(anime => anime.anime_id.toString() === animeId.toString());
+        const animeIdInSameEpisodeCLuster = animeEpisode.filter(anime => anime.cluster.toString() === selectedAnime.cluster.toString());
+        
+        const animeInSameEpisodeCLuster = animesDataset.filter(anime => {
+            const animesId = animeIdInSameEpisodeCLuster.map(a => a.anime_id);
+            return animesId.includes(anime.anime_id);
+        });
+
+        const animeFilteredByAge = animeInSameEpisodeCLuster.filter(anime => anime.rating.includes("G - All Ages"));
+
+        await userBasedRecommendation(animeId, 2000, (allMostViewed) => {
+            const animeFilteredByAgeMostViewed = allMostViewed.filter(anime => animeFilteredByAge.includes(anime));
+            console.log(animeFilteredByAgeMostViewed);
+            setInSameEpisodeCluster(randomAndLimitArray(animeFilteredByAgeMostViewed, 6));
+        })
     }
 
     const randomAndLimitArray = (arr, limit) => {
@@ -93,7 +118,8 @@ const Home = () => {
 
         const selectedAnimeInNeighbors = contentBasedRecommendation(animeId);
         contentBasedRecommendationClustering(selectedAnimeInNeighbors);
-        await userBasedRecommendation(animeId);
+        await userBasedRecommendation(animeId)
+        await episodeAgeAndUsersBasedRecommendation(animeId);
     }
 
     return (
@@ -129,6 +155,17 @@ const Home = () => {
                                                       scored_by={anime.scored_by}/>)
                                         }
                                     </div>
+                                    <h3>Ceux qui ont regardé <b>{selectedAnime.title}</b> ont aussi regardé</h3>
+                                    <div className={"recommendations"}>
+                                        {
+                                            mostViewed.map(anime =>
+                                                <Card title={anime.title} category={anime.genre}
+                                                      image={anime.image_url.replace("myanimelist.cdn-dena.com", "cdn.myanimelist.net")}
+                                                      episodes={anime.episodes}
+                                                      score={anime.score}
+                                                      scored_by={anime.scored_by}/>)
+                                        }
+                                    </div>
                                     <h3>Vous pourriez aussi aimer</h3>
                                     <div className={"recommendations"}>
                                         {
@@ -140,10 +177,10 @@ const Home = () => {
                                                       scored_by={anime.scored_by}/>)
                                         }
                                     </div>
-                                    <h3>Ceux qui ont regardé <b>{selectedAnime.title}</b> ont aussi regardé</h3>
+                                    <h3>Ceux qui ont regardé <b>{selectedAnime.title}</b> ont aussi regardé ces animes pour tous les âges</h3>
                                     <div className={"recommendations"}>
                                         {
-                                            mostViewed.map(anime =>
+                                            inSameEpisodeCluster.map(anime =>
                                                 <Card title={anime.title} category={anime.genre}
                                                       image={anime.image_url.replace("myanimelist.cdn-dena.com", "cdn.myanimelist.net")}
                                                       episodes={anime.episodes}
